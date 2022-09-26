0b47e7a0d3f910be82734ce15a09bebd1d39d450
ElenaLatynina <latynina.elena.v@gmail.com>
2022-09-26 17:55:41 +0300

    task 3-3

--- src/components/Friend/Friend.jsx
+++ src/components/Friend/Friend.jsx
@@ -3,18 +3,17 @@ import {Item, Status, Avatar, Name } from "./Friend.styled";
 
 
 
-export const FriendListItem = ({ friends: { avatar, name, isOnline } }) => {
+export const FriendListItem = ({avatar, name, isOnline}) => {
     return (<Item>
         <Status>isOnline={isOnline}</Status>
         <Avatar src={avatar} alt="User avatar" width="48" />
         <Name>{name}</Name>
     </Item>);
- 
      ; 
 };
 
 FriendListItem.propTypes = {
-    friends: PropTypes.shape({
+    friend: PropTypes.shape({
         name: PropTypes.number.isRequired,
         avatar: PropTypes.string.isRequired,
         isOnline: PropTypes.bool.isRequired,
